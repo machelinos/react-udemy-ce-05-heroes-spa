d9874c8a1e6c1576daa6dbf5c26e714b401c7e12
Marcel Cabrera <marcel.cabrera@gmail.com>
2023-06-23 17:09:19 -0600

    Heroes App - useNavigate

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,7 +1,19 @@
+import { useNavigate } from 'react-router-dom'
+
 export const LoginPage = () => {
+  const navigate = useNavigate()
+
+  const handleLogin = () => {
+    navigate('/', { replace: true })
+  }
   return (
-    <>
+    <div className="container mt-4">
       <h1>Login Page</h1>
-    </>
+      <hr />
+
+      <button className="btn btn-primary" onClick={handleLogin}>
+        Login
+      </button>
+    </div>
   )
 }
--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,6 +1,11 @@
-import { Link, NavLink } from 'react-router-dom'
+import { Link, NavLink, useNavigate } from 'react-router-dom'
 
 export const Navbar = () => {
+  const navigate = useNavigate()
+
+  const handleLogout = () => {
+    navigate('/login', { replace: true })
+  }
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
       <Link className="navbar-brand" to="/">
@@ -23,7 +28,9 @@ export const Navbar = () => {
         <div className="navbar-nav ml-auto">
           <span className="nav-item nav-link text-primary">Joy</span>
 
-          <button className="nav-item nav-link btn">Logout</button>
+          <button className="nav-item nav-link btn" onClick={handleLogout}>
+            Logout
+          </button>
         </div>
       </div>
     </nav>
