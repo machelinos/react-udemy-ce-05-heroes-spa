081f1e653637e60dee56e242c2200f83da33731d
Marcel Cabrera <marcel.cabrera@gmail.com>
2023-06-25 01:30:29 -0600

    Heroes App - Hero page

--- src/heroes/components/HeroList.jsx
+++ src/heroes/components/HeroList.jsx
@@ -1,9 +1,10 @@
 import PropTypes from 'prop-types'
 import { getHeroesByPublisher } from '../helpers'
 import { HeroCard } from './HeroCard'
+import { useMemo } from 'react'
 
 export const HeroList = ({ publisher }) => {
-  const heroes = getHeroesByPublisher(publisher)
+  const heroes = useMemo(() => getHeroesByPublisher(publisher), [publisher])
   return (
     <div className="row row-cols-1 row-cols-md-3 g-3">
       {heroes.map((hero) => (
--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,7 +1,52 @@
+import { useMemo } from 'react'
+import { Navigate, useNavigate, useParams } from 'react-router-dom'
+import { getHeroById } from '../helpers'
+
 export const HeroPage = () => {
+  const { id } = useParams()
+  const navigate = useNavigate()
+  const hero = useMemo(() => getHeroById(id), [id])
+
+  const handleBackBtnClick = () => {
+    navigate(-1)
+  }
+
+  if (!hero) return <Navigate to={'/marvel'} />
+
   return (
-    <>
-      <h1>Hero Page</h1>
-    </>
+    <div className="row mt-5">
+      <div className="col-4">
+        <img
+          className="img-thumbnail"
+          src={`/assets/heroes/${id}.jpg`}
+          alt={hero.superhero}
+        />
+      </div>
+
+      <div className="col-8">
+        <h3>{hero.superhero}</h3>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            <strong>Alter ego:</strong> {hero.alter_ego}
+          </li>
+          <li className="list-group-item">
+            <strong>Publisher:</strong> {hero.publisher}
+          </li>
+          <li className="list-group-item">
+            <strong>First appeareance:</strong> {hero.first_appearance}
+          </li>
+        </ul>
+
+        <h5 className="mt-3">Characters</h5>
+        <p>{hero.characters}</p>
+
+        <button
+          className="btn btn-outline-primary"
+          onClick={handleBackBtnClick}
+        >
+          Back
+        </button>
+      </div>
+    </div>
   )
 }
